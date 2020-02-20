e6d800cb2ef1ca7f8d96d98c2e70dd6f91346327
guruathwal <gurmeet.athwal@gmail.com>
2020-02-20 11:16:35 +0530

    translate comments to english in popup.c & disable ST7920_FULLSCREEN by default

--- TFT/src/User/Configuration.h
+++ TFT/src/User/Configuration.h
@@ -22,7 +22,7 @@
 //#define ST7920_BANNER_TEXT "LCD12864 Simulator"
 
 // Run Marlin Mode fullscreen. Not recommended for TFT24.
-#define ST7920_FULLSCREEN
+//#define ST7920_FULLSCREEN
 
 //===========================================================================
 //============================ TFT Mode Settings ============================
